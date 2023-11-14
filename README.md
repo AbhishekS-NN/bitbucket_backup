340219fdaa29280a3d1b6863a33f7ed9976311cc
Abhishek Shekar <abhishek.shekar@eke.fi>
2023-11-14 22:22:02 +0200

    Added the backup script and README.md files

---
+++ README.md
@@ -0,0 +1,17 @@
+The bash script in the repository can be used to backup the repositories in a workspace. This can be used by BitBucket admins to run a regular backup of all the repositories in the workspace. 
+
+# SCRIPT USAGE
+- Open ```bitbucket_backup.sh``` and add values to the variables:
+  ```
+  - USERNAME : BitBucket username. If the user is an admin or have access to all the repositories, then the backup will contain all the repositories in the workspace.
+  - PASSWD : App password of the user. [App password can be created using this](https://support.atlassian.com/bitbucket-cloud/docs/create-an-app-password/). The App password should have read permission to all the repositories.
+  - WORKSPACE_ID : BitBucket workspace ID.
+  - BACKUP_DIR : Directory where the backup should be present. It can be an absolute path or relative path.
+  ```
+- Add execute permission for the script
+  ```chmod 755 bitbucket_backup.sh```
+- Execute the script
+  ```./bitbucket_backup.sh```
+
+> [!NOTE]
+> Time for completition of the script depends on the number of repositories present in the workspace. If the number of repositories are more, it's a recommended to run the script in background.
---
+++ bitbucket_backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERNAME="" # BitBucket username
+PASSWD="" # App password with API permission (https://support.atlassian.com/bitbucket-cloud/docs/create-an-app-password/)
+WORKSPACE_ID="" # Workspace ID
+BACKUP_DIR="" # Backup directory
+
+# Find number of repositories
+repository_count=$(curl -s -u ${USERNAME}:${PASSWD} -X GET https://api.bitbucket.org/2.0/repositories/${WORKSPACE_ID}?fields=size | jq '.size')
+echo "Repository count: ${repository_count}"
+
+# Number of pages
+page_count=$((repository_count / 100 + 1))
+
+# Get repositories full name
+i=1
+while [ $i -le $page_count ]
+do
+    curl -s -u ${USERNAME}:${PASSWD} "https://api.bitbucket.org/2.0/repositories/${WORKSPACE_ID}?fields=values.full_name&pagelen=100&page=$i" | jq '.values[] | "\(.project.key),\(.full_name)"' >> ${BACKUP_DIR}/repos.csv
+    i=$((i+1))
+done
+
+# Backup repos
+while read -r repo
+do
+    project=$(echo "${repo}" | cut -d',' -f1)
+    echo "Back-up ${repo} in project ${project}"
+    directory="${BACKUP_DIR}/${project}/${repo}"
+    if [ -d "${directory}" ]
+    then
+        echo "Repository backup exists.... Updating backup"
+        cd ${directory}
+        git remote update
+        cd -
+    else
+        echo "Repository ${repo} backup does not exists... Cloning"
+        git clone --bare -q "https://${USERNAME}:${PASSWD}@bitbucket.org/${repo}.git" "${directory}"
+    fi
+done < ${BACKUP_DIR}/repos.csv
+
+rm -rf ${BACKUP_DIR}/repos.csv
